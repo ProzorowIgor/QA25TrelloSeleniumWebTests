9e7e96206d98af8235f44575e38ee0a247d55ba4
Igor <igor.prozorow@qualitestgroup.com>
2022-03-17 18:33:04 +0200

    dfg

--- src/test/java/com/telRan/tests/tests/LoginTrelloTests.java
+++ src/test/java/com/telRan/tests/tests/LoginTrelloTests.java
@@ -1,16 +1,25 @@
 package com.telRan.tests.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTrelloTests extends  TestBase{
-
+    ExtentReports extent = new ExtentReports();
+    ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+        //extent.attachReporter(spark);
 
     @BeforeMethod
     public void ensurePreconditions(){
-       if(app.header().isAvatarPresent()){
+        if(app.header().isAvatarPresent()){
            app.header().logout();
+            extent.attachReporter(spark);
+            extent.createTest("Before method")
+                    .log(Status.PASS, "Logout had done");
+            extent.flush();
        }
     }
 
@@ -21,7 +30,10 @@ public class LoginTrelloTests extends  TestBase{
         //er userLoggedIn (isAvatar present)
         Assert.assertTrue(app.header().isAvatarPresent());
         //is user correct
-
+        extent.attachReporter(spark);
+        extent.createTest("Login test")
+                .log(Status.PASS, "Possitive");
+        extent.flush();
     }
 
     @Test
@@ -29,6 +41,10 @@ public class LoginTrelloTests extends  TestBase{
         app.session().login("rochman.elena@gmail.com", "12345.coM");
         Assert.assertTrue(app.session().isLoginErrorPresent());
         Assert.assertFalse(app.header().isAvatarPresent());
+        extent.attachReporter(spark);
+        extent.createTest("Negative login")
+                .log(Status.PASS, "Login impossible");
+        extent.flush();
 
 
     }
